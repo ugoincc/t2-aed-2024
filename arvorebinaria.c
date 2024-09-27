79af0aa0184803a96858b538a67cd3f368ec61d0
ugoincc <hugo.incc@hotmail.com>
2024-09-27 09:20:17 -0300

    primeiro commit

---
+++ arvorebinaria.c
@@ -0,0 +1,5 @@
+//
+// Created by Hugo on 27/09/2024.
+//
+
+#include "arvorebinaria.h"
---
+++ arvorebinaria.h
@@ -0,0 +1,8 @@
+//
+// Created by Hugo on 27/09/2024.
+//
+
+#ifndef ARVOREBINARIA_H
+#define ARVOREBINARIA_H
+
+#endif //ARVOREBINARIA_H
